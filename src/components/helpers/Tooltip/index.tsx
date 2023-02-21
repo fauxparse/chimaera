bc665e3d8a889ec802bf54ba1338816861810b83
Matt Powell <matt.powell@optimalworkshop.com>
2023-02-21 15:32:09 +1300

    tooltip

---
+++ src/components/helpers/Tooltip/index.tsx
@@ -0,0 +1,7 @@
+import Tooltip from './Tooltip';
+import { TooltipHandle, TooltipProps } from './Tooltip.types';
+
+export { Tooltip };
+export type { TooltipHandle, TooltipProps };
+
+export default Tooltip;
